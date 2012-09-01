1cbf8dbf7dac2cec0643e7e1f5fb54bb057d8fe9
Kevin Nennig <knennig213@gmail.com>
2012-08-31 21:22:44 -0700

    Updated buttons too look more exciting. I also moved around the layouts on the practice and viewer to make it look better. Purely asthetics today, but still successful.

--- gen/com/example/photomemory/R.java
+++ gen/com/example/photomemory/R.java
@@ -11,8 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int green_button=0x7f020000;
+        public static final int ic_action_search=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int red_button=0x7f020003;
     }
     public static final class id {
         public static final int ButtonContainer=0x7f070006;
@@ -88,5 +90,6 @@ public final class R {
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
+        public static final int ButtonText=0x7f050001;
     }
 }
